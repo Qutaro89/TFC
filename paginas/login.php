b963f12810263f66736f6eb5b929394ff8d21307
Qutaro89 <157788291+Qutaro89@users.noreply.github.com>
2025-04-30 10:27:58 +0200

    Add files via upload

---
+++ paginas/login.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <style>
+        img{
+            width: 10%;
+            height: auto;
+        }
+        #contenedor{
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            padding: .613rem .938rem;
+            --bs-gutter-x: 1.5rem;
+            --bs-gutter-y: 0;
+            width: 50%;
+            padding-right: calc(var(--bs-gutter-x) * .5);
+            padding-left: calc(var(--bs-gutter-x) * .5);
+            margin-right: auto;
+            margin-left: auto;
+        }
+        #registro{
+            position: relative;
+            background-color: #E21921;
+            color: white;
+            transition: background-color 200ms ease-in;
+            transition: color 200ms ease-in;
+            font-size: 1rem;
+            line-height: .875rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            cursor: pointer;
+            height: 56px;
+            
+        }
+        #registro:hover{
+            background-color: #BC0A11;
+        }
+        table{
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            padding: .613rem .938rem;
+            --bs-gutter-x: 1.5rem;
+            --bs-gutter-y: 0;
+            width: 50%;
+            padding-right: calc(var(--bs-gutter-x) * .5);
+            padding-left: calc(var(--bs-gutter-x) * .5);
+            margin-right: auto;
+            margin-left: auto;
+        }
+        #enviar{
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            padding: .613rem .938rem;
+            --bs-gutter-x: 1.5rem;
+            --bs-gutter-y: 0;
+            width: 50%;
+            padding-right: calc(var(--bs-gutter-x) * .5);
+            padding-left: calc(var(--bs-gutter-x) * .5);
+            margin-right: auto;
+            margin-left: auto;
+            position: relative;
+            border: 1px solid #E21921;
+            color: #E21921;
+            transition: background-color 200ms ease-in;
+            transition: color 200ms ease-in;
+            font-size: 1rem;
+            line-height: .875rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            cursor: pointer;
+            height: 56px;
+        }
+        #enviar:hover{
+            border: 1px solid #BC0A11;
+            color: #BC0A11;
+        }
+    </style>
+</head>
+<body>
+<div id="contenedor">
+<form method="POST" action="session.php">
+<div id="logo"><a href="inicio.html"><img src="recursos/img/main-logo.png"></a></div>
+
+    <div id="registro"><p>REGÍSTRESE AHORA</p></div>
+    <h2>Inicia sesión con tu email</h2>
+    <table>
+        <tr>
+            <td><label for="user">Correo electrónico</label></td>
+            <td><input name="user" type="text" id="user"></td>
+            <td rowspan="2";><?php if(isset($_GET['err']) && ($_GET['err']==1)){echo "<h2> Contraseña incorrecta </h2>";}?></td>
+        </tr>
+        <tr>
+            <td><label for="pw">Contraseña</label></td>
+            <td><input name="pw" type="password" id="pw"></td>
+        </tr>
+    </table>
+    <input name="enviar" type="submit" id="enviar" value="Iniciar sesión">
+</form>
+</div>
+</body>
+</html>
