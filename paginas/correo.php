dd0906ad93dcf05089e2240dfda9ab6b10628c90
Adribf35 <157789103+Adribf35@users.noreply.github.com>
2025-06-11 11:59:21 +0200

    Envío de correo

--- paginas/correo.php
+++ paginas/correo.php
@@ -3,24 +3,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
-require "c:/xampp/composer/vendor/autoload.php";
-
-session_start();
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_SESSION['usuario'], $_POST['carrito'], $_POST['total'])) {
-        $usuario = $_SESSION['usuario']; // Correo del usuario
-        $carrito = $_POST['carrito'];    // Array asociativo del carrito
-        $total = $_POST['total'];        // Total de la compra
-
-        enviarCorreo($usuario, $carrito, $total);
-    } else {
-        echo "Datos incompletos para enviar el correo.";
-        exit();
-    }
-}
-
-function enviarCorreo($usuario, $carrito, $total)
+function enviarCorreo($usuario, $carrito, $total, $datos_partido)
 {
     $mail = new PHPMailer(TRUE);
     $mail->IsSMTP();
@@ -29,44 +12,67 @@ function enviarCorreo($usuario, $carrito, $total)
     $mail->SMTPSecure = "tls"; // Para usar TLS
     $mail->Host = "smtp.gmail.com";    // Servidor SMTP de Gmail
     $mail->Port = 587;                // Puerto SMTP de Gmail para TLS
-    $mail->Username = "dawiesventura@gmail.com";  // Usuario de Gmail
-    $mail->Password = "nnli oyoa game qmoc";      // Contraseña de aplicación de Gmail
-    $mail->SetFrom('adrianbf035@gmail.com', 'ADRISPORTS');
-    $mail->Subject = 'Pedido realizado - ADRISPORTS';
+    $mail->Username = "daw.ventura2023@gmail.com";  // Usuario de Gmail
+    $mail->Password = "ypxh vcue zcix ahdq";      // Contraseña de aplicación de Gmail
+    $mail->SetFrom('daw.ventura2023@gmail.com', 'Rayo Vallecano de Madrid');
+    $mail->Subject = 'Tus entradas - Rayo Vallecano de Madrid';
+
+    // Formatear fecha y hora del partido
+    $fechaHora = new DateTime($datos_partido['FECHA_HORA_PARTIDO']);
+    $fecha_es = $fechaHora->format('d/m/Y');
+    $hora_es = $fechaHora->format('H:i');
 
     // Generar el contenido del correo
-    $contenido = "<h1>Gracias por realizar tu pedido en ADRISPORTS</h1>";
-    $contenido .= "<p>A continuación, te mostramos los detalles de tu pedido:</p>";
+    $contenido = "<h1>¡Gracias por tu compra en el Rayo Vallecano!</h1>";
+    $contenido .= "<p>Aquí tienes el resumen de tus entradas:</p>";
+    $contenido .= "<table border='1' cellpadding='5' cellspacing='0' style='border-collapse: collapse; width: 100%; margin-bottom: 20px;'>";
+    $contenido .= "<thead>
+                    <tr style='background-color: #f2f2f2;'>
+                        <th>Partido</th>
+                        <th>Fecha y Hora</th>
+                        <th>Jornada</th>
+                    </tr>
+                   </thead>
+                   <tbody>
+                    <tr>
+                        <td>" . htmlspecialchars($datos_partido['EQUIPO_LOCAL']) . " vs " . htmlspecialchars($datos_partido['EQUIPO_VISITANTE']) . "</td>
+                        <td>" . $fecha_es . " - " . $hora_es . "</td>
+                        <td>" . htmlspecialchars($datos_partido['ID_PARTIDO']) . "</td>
+                    </tr>
+                   </tbody>
+                  </table>";
+
+    $contenido .= "<h3>Detalles de las Butacas:</h3>";
     $contenido .= "<table border='1' cellpadding='5' cellspacing='0' style='border-collapse: collapse; width: 100%;'>";
     $contenido .= "<thead>
-                    <tr>
-                        <th>Producto</th>
-                        <th>Unidades</th>
-                        <th>Precio unitario</th>
-                        <th>Subtotal</th>
+                    <tr style='background-color: #f2f2f2;'>
+                        <th>ID Butaca</th>
+                        <th>Zona</th>
+                        <th>Puerta</th>
+                        <th>Precio</th>
                     </tr>
                    </thead>
                    <tbody>";
 
-    foreach ($carrito as $producto) {
-        $nombre = htmlspecialchars($producto['nombre']);
-        $cantidad = (int)$producto['cantidad'];
-        $precio = number_format((float)$producto['precio'], 2);
-        $subtotal = number_format((float)$producto['subtotal'], 2);
+    foreach ($carrito as $butaca_data) {
+        $id_butaca = htmlspecialchars($butaca_data['id_butaca']);
+        $zona = htmlspecialchars($butaca_data['zona']);
+        $puerta = htmlspecialchars($butaca_data['puerta']);
+        $precio_unitario = number_format((float)$butaca_data['precio_unitario'], 2);
 
         $contenido .= "<tr>
-                            <td>{$nombre}</td>
-                            <td>{$cantidad}</td>
-                            <td>{$precio}€</td>
-                            <td>{$subtotal}€</td>
+                            <td>{$id_butaca}</td>
+                            <td>{$zona}</td>
+                            <td>{$puerta}</td>
+                            <td>{$precio_unitario}€</td>
                         </tr>";
     }
 
     $contenido .= "</tbody>";
     $contenido .= "<tfoot>
-                    <tr>
-                        <td colspan='3'><strong>Total:</strong></td>
-                        <td><strong>" . number_format((float)$total, 2) . "€</strong></td>
+                    <tr style='background-color: #f2f2f2; font-weight: bold;'>
+                        <td colspan='3' style='text-align: right;'>Total de la Compra:</td>
+                        <td>" . number_format((float)$total, 2) . "€</td>
                     </tr>
                    </tfoot>";
     $contenido .= "</table>";
